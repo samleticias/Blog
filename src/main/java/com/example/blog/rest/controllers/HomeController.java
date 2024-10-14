92642f21e4f2d956bf14bb9a6dc625636ed0258a
samleticias <sammyaleticia09@gmail.com>
2024-10-13 23:33:55 -0300

    feat: initial template

---
+++ src/main/java/com/example/blog/rest/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.blog.rest.controllers;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        return "home";
+    }
+
+}
